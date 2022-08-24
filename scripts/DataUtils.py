c7bc788e2d2ceff7a5b86b6c33d9600a1a02eb96
anteneh tilaye <antenehtilaye19@gmail.com>
2022-08-24 13:27:29 +0300

    updating utils and custumizing the notebook

--- scripts/DataUtils.py
+++ scripts/DataUtils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pandas as pd
 def missing_percent(df):
 
     '''
@@ -21,4 +21,42 @@ def missing_percent(df):
     # Calculate percentage of missing values
     print("The dataset contains", round(((totalMissing/totalCells) * 100), 2), "%", "missing values.")
     
+def fill_using_median(df, cols):
 
+    '''
+        this function fill a missing data using median
+    
+        args:
+            df: DataFrame - the data frame
+            
+            cols: list - contains a list of column name to be filled
+            
+        return:
+
+            returns the filled Dataframe  
+    '''
+        
+    for name in cols:
+        df.loc[pd.isnull(df[name]), [name]] = df[name].quantile(0.75) - df[name].quantile(0.25)
+        
+    return df
+
+def fill_using_mode(df, cols):
+
+    '''
+        this function fill a missing data using mode
+    
+        args:
+            df: DataFrame - the data frame
+            
+            cols: list - contains a list of column name to be filled
+            
+        return:
+
+            returns the filled Dataframe  
+    '''
+        
+    for name in cols:
+        df[name].fillna(value=df[name].mode().iloc[0], inplace=True)
+        
+    return df
